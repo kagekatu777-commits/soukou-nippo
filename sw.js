862507b26f866a2a6038afd2ca62d38b8342e6e5
hide <kagekatu777@gmail.com>
2026-08-11 18:57:05 +0900

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,1 +1,16 @@
 
+const CACHE_NAME = "soukou-nippo-v1";
+
+self.addEventListener("install", event => {
+  self.skipWaiting();
+});
+
+self.addEventListener("activate", event => {
+  event.waitUntil(self.clients.claim());
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    fetch(event.request).catch(() => caches.match(event.request))
+  );
+});
